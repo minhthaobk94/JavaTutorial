185ed8c655c6acadac6e688de82fb7f1bca47ef5
MinhThao <minhthaobk94@gmail.com>
2017-09-01 13:28:34 +0700

    +convert bin to dec

---
+++ src/string/Bin2Dec.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.util.Scanner;
+
+public class Bin2Dec {
+	public int convertBin2Dec(String input) {
+		int dec = 0;
+		for (int i = 0; i < input.length(); i++) {
+			if(input.charAt(i) == '1') {
+				dec += Math.pow(2, input.length() - i);
+			} else if (input.charAt(i) != '0'){
+				System.out.println("Error");
+				System.exit(0);;
+			}
+		}
+		return dec;
+	}
+
+	public static void main(String[] args) {
+		Bin2Dec bin2Dec = new Bin2Dec();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Binary: ");
+		String input = scanner.nextLine();
+		System.out.println("Decimal is " + bin2Dec.convertBin2Dec(input));
+		scanner.close();
+	}
+
+}
