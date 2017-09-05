fd9589cfaaa7188c23c78ad4f337281609ebea7d
MinhThao <minhthaobk94@gmail.com>
2017-09-05 15:27:00 +0700

    +displays the following n×n (n=5) pattern using two nested for-loops

---
+++ src/flow_controls/SquareBoard.java
@@ -0,0 +1,25 @@
+package flow_controls;
+
+import java.util.Scanner;
+
+public class SquareBoard {
+
+	public void draw(int n) {
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < n; col++) {
+				System.out.print("# ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		SquareBoard squareBoard = new SquareBoard();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		int n = scanner.nextInt();
+		squareBoard.draw(n);
+		scanner.close();
+	}
+
+}
