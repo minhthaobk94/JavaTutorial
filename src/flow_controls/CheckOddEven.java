f65787b2a3f56e9de9d2cf1732d01b8c23acb04c
MinhThao <minhthaobk94@gmail.com>
2017-09-05 10:04:19 +0700

    !refactor package name

---
+++ src/flow_controls/CheckOddEven.java
@@ -0,0 +1,32 @@
+package flow_controls;
+
+import java.util.Scanner;
+/**
+ * Check odd/even a number input from keyboard
+ * @author MinhThao
+ *
+ */
+public class CheckOddEven {
+
+	/*
+	 * This is check odd/even function
+	 * Input a number (int)
+	 */
+	public void checkOddEden(int number) {
+		if(number % 2 == 1) {
+			System.out.println("Odd Number");
+		} else {
+			System.out.println("Even Number");
+		}
+	}
+	
+	public static void main(String[] args) {
+		CheckOddEven obj = new CheckOddEven();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input number you want to check: ");
+		int numberInput = Integer.parseInt(scanner.nextLine());
+		obj.checkOddEden(numberInput);
+		System.out.println("\nBYE!");
+		scanner.close();
+	}
+}
