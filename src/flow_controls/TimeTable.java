630865d2dfa3605fb59ff63b5987326a4816ed00
MinhThao <minhthaobk94@gmail.com>
2017-09-05 16:05:31 +0700

    +TimeTable to produce the multiplication table of 1 to 9 as shown using two nested for-loops

---
+++ src/flow_controls/TimeTable.java
@@ -0,0 +1,33 @@
+package flow_controls;
+
+import java.util.Scanner;
+
+public class TimeTable {
+	
+	public void draw (int n) {
+		for (int row = 1; row <= n; row++) {
+			System.out.printf("%2d %2s", row, "|");
+			for (int col = 1; col <= n; col++) {
+				int value = row * col;
+				System.out.printf(" %2d ", value);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		TimeTable timeTable = new TimeTable();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		int n = scanner.nextInt();
+		System.out.printf("%2s %2s ", "*", "|");
+		for (int i = 1; i <= n; i++) {
+			System.out.printf("%2d  ", i);
+		}
+		System.out.println("\n-------------------------------------");
+		timeTable.draw(n);
+		scanner.close();
+
+	}
+
+}
