b8357844a32b69fe854e563306936f22a509b82d
MinhThao <minhthaobk94@gmail.com>
2017-09-05 15:38:53 +0700

    +CheckerBoard that displays the following n×n (n=7) checkerboard pattern using two nested for-loops.

---
+++ src/flow_controls/CheckedBoard.java
@@ -0,0 +1,28 @@
+package flow_controls;
+
+import java.util.Scanner;
+
+public class CheckedBoard {
+
+	public void draw(int n) {
+		for (int row = 0; row < n; row++) {
+			if (row % 2 != 0) {
+				System.out.print(" ");
+			}
+			for (int col = 0; col < n; col++) {
+				System.out.print("# ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		CheckedBoard checkedBoard = new CheckedBoard();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		int n = scanner.nextInt();
+		checkedBoard.draw(n);
+		scanner.close();
+	}
+
+}
