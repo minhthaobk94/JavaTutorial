ed1bb1415b5f303afdb350b52d1e819ca424bf2c
MinhThao <minhthaobk94@gmail.com>
2017-09-05 10:26:54 +0700

    +Compute 1 to N, N is input

---
+++ src/flow_controls/Product1ToN.java
@@ -0,0 +1,24 @@
+package flow_controls;
+
+import java.util.Scanner;
+
+public class Product1ToN {
+	
+	public void compute(int n) {
+		long product = 1;
+		for (int i = 1; i <= n; i++) {
+			product *= i;
+		}
+		System.out.printf("Product 1 to %d is %d", n, product);
+	}
+
+	public static void main(String[] args) {
+		Product1ToN product1ToN = new Product1ToN();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter N: ");
+		int n = scanner.nextInt();
+		product1ToN.compute(n);
+		scanner.close();
+	}
+
+}
