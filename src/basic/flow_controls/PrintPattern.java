33a3dfa079518f272e081ab36834524409de1f9c
MinhThao <minhthaobk94@gmail.com>
2017-09-06 13:52:36 +0700

    !draw more

--- src/basic/flow_controls/PrintPattern.java
+++ src/basic/flow_controls/PrintPattern.java
@@ -12,15 +12,43 @@ public class PrintPattern {
 			System.out.println();
 		}
 	}
+
+	public void drawB(int n) {
+		for (int row = 0; row < n; row++) {
+			for (int col = n - 1; col >= row; col--) {
+				System.out.printf("%2s", "#");
+			}
+			System.out.println();
+		}
+	}
+
+//	public void drawC(int n) {
+//		for(int row = 0; row < n; row++) {
+//			for (int col = row; col < n; col++) {
+//				System.out.printf("%2s", " ");
+//			}
+//			System.out.println();
+//		}
+//	}
 	
-	
+	public void drawC(int n) {
+		for (int row = n - 1; row >= 0; row--) {
+			for (int col = n - 1; col > row; col--) {
+				System.out.printf("%2s", " ");
+			}
+			for (int k = 0; k <= row; k++) {
+				System.out.printf("%2s", "#");
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		PrintPattern printPattern = new PrintPattern();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter number: ");
 		int n = scanner.nextInt();
-		printPattern.drawA(n);
+		printPattern.drawC(n);
 		scanner.close();
 	}
 
